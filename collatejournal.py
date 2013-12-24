f68c95044bdb1b99b8870fce84a1322b26220062
Nathanael Farley <nasfarley88@gmail.com>
2013-12-24 21:01:45 +0000

    closed the file to allow pdflatex to work it's magic

--- collatejournal.py
+++ collatejournal.py
@@ -2,13 +2,15 @@
 
 # For naming the journal temporary file
 from time import strftime
+
+from subprocess import call
 import os
 
 # filename string, always good to have!
-filename = "journal" + strftime("%FT%H%M%S") + ".tex"
+filename = "journal" + strftime("%FT%H%M%S") 
 
 # Open file ready for writing
-f = open(filename,"w+")
+f = open(filename+".tex","w+")
 
 beginningOfFile = r"""\documentclass{memoir}
 
@@ -22,6 +24,8 @@ middleOfFile = r"""I don't think the import worked \ldots"""
 # TODO import the text files here
 
 f.write(beginningOfFile + middleOfFile + endOfFile)
+f.close()
 
 print "Attempting to run pdflatex"
-os.system("pdflatex " + filename)
+
+call(['pdflatex',filename])
