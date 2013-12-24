3c95478f3135f0ceb881ca90e34f949a69529f9e
Nathanael Farley <nasfarley88@gmail.com>
2013-12-24 20:15:22 +0000

    Added writing of LaTeX temporary file

--- collatejournal.py
+++ collatejournal.py
@@ -1,3 +1,27 @@
 #!/usr/bin/python
 
-print("Hello World")
+# For naming the journal temporary file
+from time import strftime
+import os
+
+# filename string, always good to have!
+filename = "journal" + strftime("%FT%H%M%S")
+
+# Open file ready for writing
+f.open("filename","w+")
+
+beginningOfFile = """\documentclass{memoir}
+
+\begin{document}"""
+
+endOfFile = """
+\end{document}"""
+
+middleOfFile = """I don't think the import worked \ldots"""
+
+# TODO import the text files here
+
+f.write(beginningOfFile + middleOfFile + endOfFile)
+
+print "Attempting to run pdflatex"
+os.system("pdflatex " + filename)
